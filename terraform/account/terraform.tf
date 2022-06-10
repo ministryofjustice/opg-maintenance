e3e6f1a5d8711c4d75885761c2a0fa1fb3d031f3
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-10 13:34:42 +0100

    initialise account path

---
+++ terraform/account/terraform.tf
@@ -0,0 +1,44 @@
+terraform {
+  backend "s3" {
+    bucket         = "opg.terraform.state"
+    key            = "opg-maintenance-account/terraform.tfstate"
+    encrypt        = true
+    region         = "eu-west-1"
+    role_arn       = "arn:aws:iam::311462405659:role/opg-maintenance-ci"
+    dynamodb_table = "remote_lock"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.18.0"
+    }
+  }
+  required_version = ">= 1.2.2"
+}
+
+variable "default_role" {
+  type    = string
+  default = "opg-maintenance-ci"
+}
+variable "management_role" {
+  type    = string
+  default = "opg-maintenance-ci"
+}
+
+provider "aws" {
+  alias  = "eu_west_2"
+  region = "eu-west-2"
+  assume_role {
+    role_arn     = "arn:aws:iam::${local.account.account_id}:role/${var.default_role}"
+    session_name = "opg-maintenance-terraform-session"
+  }
+}
+
+provider "aws" {
+  alias  = "management"
+  region = "eu-west-2"
+  assume_role {
+    role_arn     = "arn:aws:iam::311462405659:role/${var.management_role}"
+    session_name = "opg-maintenance-terraform-session"
+  }
+}
