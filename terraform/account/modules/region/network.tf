43fc4a3b4da2abf365db072d222601b86b369400
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-14 13:28:43 +0100

    use v1.0.0 of the networks module

--- terraform/account/modules/region/network.tf
+++ terraform/account/modules/region/network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source                         = "git@github.com:ministryofjustice/opg-terraform-aws-network.git?ref=v0.2.0-UML-2475.5"
+  source                         = "git@github.com:ministryofjustice/opg-terraform-aws-network.git?ref=v1.0.0"
   cidr                           = "10.162.0.0/16"
   default_security_group_ingress = [{}]
   default_security_group_egress  = [{}]
