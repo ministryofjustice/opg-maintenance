0ec007fef106743daae5595dcd60620ef747142a
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-14 14:39:02 +0100

    make an input variable for cidr range

--- terraform/account/modules/region/network.tf
+++ terraform/account/modules/region/network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source                         = "github.com/ministryofjustice/opg-terraform-aws-network?ref=v1.0.0"
-  cidr                           = "10.162.0.0/16"
+  cidr                           = var.network_cidr_block
   default_security_group_ingress = [{}]
   default_security_group_egress  = [{}]
   providers = {
--- terraform/account/regions.tf
+++ terraform/account/regions.tf
@@ -1,5 +1,6 @@
 module "eu_west_2" {
-  source = "./modules/region"
+  source             = "./modules/region"
+  network_cidr_block = "10.162.0.0/16"
   providers = {
     aws.region = aws.eu_west_2
   }
