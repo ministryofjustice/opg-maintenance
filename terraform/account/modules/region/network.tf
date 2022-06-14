8378728cba6891c24fdafc6577bccfed6895b75d
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-14 14:00:52 +0100

    use https to pull module

--- terraform/account/modules/region/network.tf
+++ terraform/account/modules/region/network.tf
@@ -1,5 +1,6 @@
 module "network" {
-  source                         = "git@github.com:ministryofjustice/opg-terraform-aws-network.git?ref=v1.0.0"
+  source = "github.com/ministryofjustice/opg-terraform-aws-network?ref=v1.0.0"
+  # source                         = "git@github.com:ministryofjustice/opg-terraform-aws-network.git?ref=v1.0.0"
   cidr                           = "10.162.0.0/16"
   default_security_group_ingress = [{}]
   default_security_group_egress  = [{}]
