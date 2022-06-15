9148cc2d2c39709dc213c5aa957788055ac5446e
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-15 12:09:11 +0100

    add cloudwatch log group for application logs

--- terraform/account/regions.tf
+++ terraform/account/regions.tf
@@ -1,6 +1,9 @@
 module "eu_west_2" {
-  source             = "./modules/region"
-  network_cidr_block = "10.162.0.0/16"
+  source                         = "./modules/region"
+  network_cidr_block             = "10.162.0.0/16"
+  application_log_retention_days = local.account.cloudwatch_log_groups.application_log_retention_days
+  application_name               = local.mandatory_moj_tags.application
+  account_name                   = local.mandatory_moj_tags.environment-name
   providers = {
     aws.region = aws.eu_west_2
   }
