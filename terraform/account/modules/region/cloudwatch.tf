2f8297c2c72a3b2e586b567762deb056c191c1bb
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-15 12:25:29 +0100

    add app name to kms key description

--- terraform/account/modules/region/cloudwatch.tf
+++ terraform/account/modules/region/cloudwatch.tf
@@ -9,7 +9,7 @@ resource "aws_cloudwatch_log_group" "application" {
 }
 
 resource "aws_kms_key" "cloudwatch" {
-  description             = "Cloudwatch application logs encryption key"
+  description             = "${var.application_name} cloudwatch application logs encryption key"
   deletion_window_in_days = 10
   enable_key_rotation     = true
   policy                  = data.aws_iam_policy_document.cloudwatch_kms.json
