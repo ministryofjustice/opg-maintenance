61ebf509990b4f2990898da4d638c700e6e69e02
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-16 15:09:39 +0100

    set value

--- terraform/environment/regions.tf
+++ terraform/environment/regions.tf
@@ -1,7 +1,7 @@
 module "eu_west_2" {
   source                                = "./modules/region"
-  ecs_cluster_container_insights        = local.ecs_capacity_provider
-  maintenance_service_capacity_provider = local.cluster_container_insights
+  ecs_cluster_container_insights        = local.cluster_container_insights
+  maintenance_service_capacity_provider = local.ecs_capacity_provider
   maintenance_service_repository_url    = data.aws_ecr_repository.maintenance_app.repository_url
   maintenance_service_container_version = var.container_version
   application_log_retention_days        = local.environment.cloudwatch_log_groups.application_log_retention_days
