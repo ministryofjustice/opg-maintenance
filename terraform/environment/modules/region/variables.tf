d294021e4f48a10599b65e685de2be826d7221bb
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-16 15:02:54 +0100

    provision ecs service

--- terraform/environment/modules/region/variables.tf
+++ terraform/environment/modules/region/variables.tf
@@ -1,8 +1,28 @@
 locals {
-  name_prefix = "${data.aws_default_tags.current.tags.application}-${data.aws_default_tags.current.tags.environment-name}"
+  name_prefix = "${data.aws_default_tags.current.tags.application}-${data.aws_default_tags.current.tags.environment-name}-${data.aws_region.current.name}"
 }
 
 variable "maintenance_service_capacity_provider" {
   type        = string
   description = "Name of the capacity provider to use. Valid values are FARGATE_SPOT and FARGATE"
+}
+
+variable "ecs_cluster_container_insights" {
+  type        = string
+  description = "value for containerInsights setting on ecs cluster. Valid values are enabled and disabled"
+}
+
+variable "maintenance_service_container_version" {
+  type        = string
+  description = "Tag of the maintenance app container to deploy"
+}
+
+variable "maintenance_service_repository_url" {
+  type        = string
+  description = "ECR repository url for the maintenance app container"
+}
+
+variable "application_log_retention_days" {
+  type        = number
+  description = "Specifies the number of days you want to retain log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653, and 0. If you select 0, the events in the log group are always retained and never expire."
 }
--- terraform/environment/regions.tf
+++ terraform/environment/regions.tf
@@ -1,7 +1,16 @@
 module "eu_west_2" {
   source                                = "./modules/region"
-  maintenance_service_capacity_provider = local.environment.ecs.maintenance_service_capacity_provider
+  ecs_cluster_container_insights        = local.ecs_capacity_provider
+  maintenance_service_capacity_provider = local.cluster_container_insights
+  maintenance_service_repository_url    = data.aws_ecr_repository.maintenance_app.repository_url
+  maintenance_service_container_version = var.container_version
+  application_log_retention_days        = local.environment.cloudwatch_log_groups.application_log_retention_days
   providers = {
     aws.region = aws.eu_west_2
   }
+}
+
+data "aws_ecr_repository" "maintenance_app" {
+  name     = "maintenance/maintenance_app"
+  provider = aws.management
 }
