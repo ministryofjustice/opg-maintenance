7ecf6853bc549844c8be5e4827ffe1ae1cc58eb8
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-17 13:52:04 +0100

    fix validation record name

--- terraform/account/modules/region/certificate.tf
+++ terraform/account/modules/region/certificate.tf
@@ -7,7 +7,7 @@ resource "aws_route53_record" "certificate_validation_maintenance" {
   provider = aws.management
   for_each = {
     for dvo in aws_acm_certificate.certificate_maintenance.domain_validation_options : dvo.domain_name => {
-      name   = "${dvo.resource_record_name}-${data.aws_region.current.name}"
+      name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
     }
