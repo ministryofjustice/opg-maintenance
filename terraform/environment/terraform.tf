dd6d4da53781b4297b734e7e0a5cb90014ea578d
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-21 20:25:06 +0100

    add provider for london mnagement account

--- terraform/environment/terraform.tf
+++ terraform/environment/terraform.tf
@@ -34,6 +34,18 @@ provider "aws" {
 }
 
 provider "aws" {
+  alias  = "management_eu_west_2"
+  region = "eu-west-2"
+  default_tags {
+    tags = local.default_tags
+  }
+  assume_role {
+    role_arn     = "arn:aws:iam::311462405659:role/${var.default_role}"
+    session_name = "opg-maintenance-terraform-session"
+  }
+}
+
+provider "aws" {
   alias  = "management"
   region = "eu-west-1"
   default_tags {
