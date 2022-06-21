7eb443d28bdbe14a87cfe12d63abc1892b09b1ab
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-21 20:27:53 +0100

    use new variables to configure dns and certificates

--- terraform/environment/regions.tf
+++ terraform/environment/regions.tf
@@ -1,13 +1,15 @@
 module "eu_west_2" {
   source                                = "./modules/region"
-  vpc_id                                = "vpc-0ea91174952db35ad"
   ecs_cluster_container_insights        = local.cluster_container_insights
   maintenance_service_capacity_provider = local.ecs_capacity_provider
   maintenance_service_repository_url    = data.aws_ecr_repository.maintenance_app.repository_url
   maintenance_service_container_version = var.container_version
   application_log_retention_days        = local.environment.cloudwatch_log_groups.application_log_retention_days
+  account_name                          = local.environment.account_name
+  enable_deletion_protection = local.environment.application_load_balancer.enable_deletion_protection
   providers = {
     aws.region = aws.eu_west_2
+    aws.management = aws.eu_west_2
   }
 }
 
