b9c6b286ff42c7e9974dbc73daab0aa881bf92fc
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-22 10:22:17 +0100

    move dns out of region

--- terraform/environment/dns.tf
+++ terraform/environment/dns.tf
@@ -1,19 +1,19 @@
 data "aws_route53_zone" "maintenance" {
-  provider = aws.management
+  provider = aws.management_eu_west_1
   name     = "maintenance.opg.service.justice.gov.uk"
 }
 
 resource "aws_route53_record" "maintenance" {
   # maintenance.opg.service.justice.gov.uk
-  provider = aws.management
+  provider = aws.management_eu_west_1
   zone_id  = data.aws_route53_zone.maintenance.zone_id
-  name     = "${local.dns_namespace_for_environment}${data.aws_route53_zone.maintenance.name}"
+  name     = "${local.environment_name}${data.aws_route53_zone.maintenance.name}"
   type     = "A"
 
   alias {
     evaluate_target_health = false
-    name                   = aws_lb.maintenance.dns_name
-    zone_id                = aws_lb.maintenance.zone_id
+    name                   = module.eu_west_2.maintenance_lb.dns_name
+    zone_id                = module.eu_west_2.maintenance_lb.zone_id
   }
 
   lifecycle {
