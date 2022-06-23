bd1d6d10cb17c50a15826e0f058ab928deb58d53
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-23 13:45:33 +0100

    enable container insights always

--- terraform/environment/modules/region/ecs_cluster.tf
+++ terraform/environment/modules/region/ecs_cluster.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_cluster" "main" {
   name = local.name_prefix
   setting {
     name  = "containerInsights"
-    value = "true"
+    value = "enabled"
   }
   provider = aws.region
 }
