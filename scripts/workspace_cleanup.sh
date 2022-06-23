a9c6ca57ff0c8e5de06de20f3cb49a374b01d502
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-23 15:15:34 +0100

    add script to clean up workspaces

---
+++ scripts/workspace_cleanup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+if [ $# -eq 0 ]
+  then
+    echo "Please provide workspaces to be removed."
+fi
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: `basename $0` [workspaces separated by a space]"
+  exit 0
+fi
+
+function getWorkspaces {
+  terraform workspace list
+}
+
+in_use_workspaces="$@"
+reserved_workspaces="default production preproduction"
+
+protected_workspaces="$in_use_workspaces $reserved_workspaces"
+all_workspaces=$(terraform workspace list|sed 's/*//g')
+
+for workspace in $all_workspaces
+do
+  case "$protected_workspaces" in
+    *$workspace*)
+      echo "protected workspace: $workspace"
+      ;;
+    *)
+      echo "cleaning up workspace $workspace..."
+      terraform workspace select $workspace
+
+      ;;
+  esac
+done
