597adde2681a795ae53bd63afcd8aa84a625a71d
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-29 14:19:20 +0100

    add log insights query

--- terraform/environment/modules/region/cloudwatch_log_groups.tf
+++ terraform/environment/modules/region/cloudwatch_log_groups.tf
@@ -9,3 +9,13 @@ resource "aws_cloudwatch_log_group" "application_logs" {
   kms_key_id        = data.aws_kms_alias.cloudwatch_application_logs_encryption.target_key_arn
   provider          = aws.region
 }
+resource "aws_cloudwatch_query_definition" "app_container_messages" {
+  name            = "Maintenance Service Application Logs/${data.aws_default_tags.current.tags.environment-name} app container messages"
+  log_group_names = [aws_cloudwatch_log_group.application_logs.name]
+
+  query_string = <<EOF
+fields @timestamp, message, concat(method, " ", url) as request, status
+| filter @message not like "ELB-HealthChecker"
+| sort @timestamp desc
+EOF
+}
