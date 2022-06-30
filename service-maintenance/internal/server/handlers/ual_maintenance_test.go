d3d0209d27e50e4be74931ced48eafbabe341370
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-06-30 10:38:22 +0100

    add tests for failing templates

--- service-maintenance/internal/server/handlers/ual_maintenance_test.go
+++ service-maintenance/internal/server/handlers/ual_maintenance_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/ministryofjustice/opg-maintenance/service-maintenance/internal/server"
 	. "github.com/ministryofjustice/opg-maintenance/service-maintenance/internal/server/handlers"
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -21,6 +22,27 @@ func TestUseALPAEnglishMaintenanceHandler(t *testing.T) {
 	assert.HTTPBodyContains(t, handler.ServeHTTP, "GET", "/en-gb/use-lasting-power-of-attorney", nil, "Use a lasting power of attorney")
 }
 
+func TestUseALPAEnglishMaintenanceHandler_WithBadTemplate(t *testing.T) {
+	t.Parallel()
+
+	memfs := afero.NewMemMapFs()
+
+	err := afero.WriteFile(memfs, "test.page.gohtml", []byte(""), 0644)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	fs := afero.NewIOFS(memfs)
+
+	handler := server.WithTemplates(
+		UseALPAWelshMaintenanceHandler(),
+		server.LoadTemplates(fs), // bad template location loads no templates
+	)
+
+	// the handler panics but that is handled upstream so it claims success at this point
+	assert.HTTPSuccess(t, handler.ServeHTTP, "GET", "/en-gb/use-lasting-power-of-attorney", nil)
+}
+
 func TestUseALPAWelshMaintenanceHandler(t *testing.T) {
 	t.Parallel()
 
@@ -32,6 +54,28 @@ func TestUseALPAWelshMaintenanceHandler(t *testing.T) {
 	assert.HTTPSuccess(t, handler.ServeHTTP, "GET", "/cy/defnyddio-atwrneiaeth-arhosol", nil)
 	assert.HTTPBodyContains(t, handler.ServeHTTP, "GET", "/cy/defnyddio-atwrneiaeth-arhosol", nil, "Defnyddio atwrneiaeth arhosol")
 }
+
+func TestUseALPAWelshMaintenanceHandler_WithBadTemplate(t *testing.T) {
+	t.Parallel()
+
+	memfs := afero.NewMemMapFs()
+
+	err := afero.WriteFile(memfs, "test.page.gohtml", []byte(""), 0644)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	fs := afero.NewIOFS(memfs)
+
+	handler := server.WithTemplates(
+		UseALPAWelshMaintenanceHandler(),
+		server.LoadTemplates(fs), // bad template location loads no templates
+	)
+
+	// the handler panics but that is handled upstream so it claims success at this point
+	assert.HTTPSuccess(t, handler.ServeHTTP, "GET", "/cy/defnyddio-atwrneiaeth-arhosol", nil)
+}
+
 func TestViewALPAEnglishMaintenanceHandler(t *testing.T) {
 	t.Parallel()
 
@@ -43,6 +87,28 @@ func TestViewALPAEnglishMaintenanceHandler(t *testing.T) {
 	assert.HTTPSuccess(t, handler.ServeHTTP, "GET", "/en-gb/view-lasting-power-of-attorney", nil)
 	assert.HTTPBodyContains(t, handler.ServeHTTP, "GET", "/en-gb/view-lasting-power-of-attorney", nil, "View a lasting power of attorney")
 }
+
+func TestViewALPAEnglishMaintenanceHandler_WithBadTemplate(t *testing.T) {
+	t.Parallel()
+
+	memfs := afero.NewMemMapFs()
+
+	err := afero.WriteFile(memfs, "test.page.gohtml", []byte(""), 0644)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	fs := afero.NewIOFS(memfs)
+
+	handler := server.WithTemplates(
+		ViewALPAEnglishMaintenanceHandler(),
+		server.LoadTemplates(fs), // bad template location loads no templates
+	)
+
+	// the handler panics but that is handled upstream so it claims success at this point
+	assert.HTTPSuccess(t, handler.ServeHTTP, "GET", "/en-gb/view-lasting-power-of-attorney", nil)
+}
+
 func TestViewALPAWelshMaintenanceHandler(t *testing.T) {
 	t.Parallel()
 
@@ -53,4 +119,25 @@ func TestViewALPAWelshMaintenanceHandler(t *testing.T) {
 
 	assert.HTTPSuccess(t, handler.ServeHTTP, "GET", "/cy/gweld-atwrneiaeth-arhosol", nil)
 	assert.HTTPBodyContains(t, handler.ServeHTTP, "GET", "/cy/gweld-atwrneiaeth-arhosol", nil, "Gweld atwrneiaeth arhosol")
+}
+
+func TestViewALPAWelshMaintenanceHandler_WithBadTemplate(t *testing.T) {
+	t.Parallel()
+
+	memfs := afero.NewMemMapFs()
+
+	err := afero.WriteFile(memfs, "test.page.gohtml", []byte(""), 0644)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	fs := afero.NewIOFS(memfs)
+
+	handler := server.WithTemplates(
+		ViewALPAWelshMaintenanceHandler(),
+		server.LoadTemplates(fs), // bad template location loads no templates
+	)
+
+	// the handler panics but that is handled upstream so it claims success at this point
+	assert.HTTPSuccess(t, handler.ServeHTTP, "GET", "/cy/gweld-atwrneiaeth-arhosol", nil)
 }
